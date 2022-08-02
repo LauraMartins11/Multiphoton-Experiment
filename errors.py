fe2ab89cc2c6198f64fe2106b086b96a1017b316
LauraMartins11 <laura_smartins@hotmail.com>
2022-08-02 13:10:13 +0200

    fixed errros.py

--- errors.py
+++ errors.py
@@ -9,31 +9,31 @@ import numpy as np
 ## the uncertainty of the WP's we are using
 HWP_DICT={"d": np.pi/8,
           "l": 0,
-          "v": 0,
+          "h": 0,
           "a": -np.pi/8,
           "r": 0,
-          "h": np.pi/4}
+          "v": np.pi/4}
 
 QWP_DICT={"d": np.pi/2,
           "l": 3*np.pi/4,
-          "v": np.pi/2,
+          "h": np.pi/2,
           "a": np.pi/2,
           "r": np.pi/4,
-          "h": np.pi/2}
+          "v": np.pi/2}
 
 PROJECTORS={"d": np.array([1,1])/np.sqrt(2),
             "l": np.array([1,1j])/np.sqrt(2),
-            "v": np.array([1,0]),
+            "h": np.array([1,0]),
             "a": np.array([1,-1])/np.sqrt(2),
             "r": np.array([1,-1j])/np.sqrt(2),
-            "h": np.array([0,1])}
+            "v": np.array([0,1])}
 
 
 OBSERVABLES=np.transpose(np.array([
-    ['dd', 'dl', 'dv', 'ld', 'll', 'lh', 'vd', 'vl', 'vv'],
-    ['da', 'dr', 'dh', 'la', 'lr', 'lh', 'va', 'vr', 'vh'],
-    ['ad', 'al', 'av', 'rd', 'rl', 'rv', 'hd', 'hl', 'hv'],
-    ['aa', 'ar', 'ah', 'ra', 'rr', 'rh', 'ha', 'hr', 'hh']])
+    ['dd', 'dl', 'dh', 'ld', 'll', 'lh', 'hd', 'hl', 'hh'],
+    ['da', 'dr', 'dv', 'la', 'lr', 'lv', 'ha', 'hr', 'hv'],
+    ['ad', 'al', 'ah', 'rd', 'rl', 'rh', 'vd', 'vl', 'vh'],
+    ['aa', 'ar', 'av', 'ra', 'rr', 'rv', 'va', 'vr', 'vv']])
     )
 
 # OBSERVABLES=['dd','da','ad','aa']
